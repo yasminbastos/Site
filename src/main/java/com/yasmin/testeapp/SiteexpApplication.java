c2223145afa35f4fc4b436479e15cf98a9737bbc
Yasmin <yasmincatherinnebastos@gmail.com>
2025-08-08 20:44:37 -0300

    first commit

---
+++ src/main/java/com/yasmin/testeapp/SiteexpApplication.java
@@ -0,0 +1,13 @@
+package com.yasmin.testeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteexpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteexpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yasmin/testeapp/SiteexpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasmin.testeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteexpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
